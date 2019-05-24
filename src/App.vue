92cc768ed6c8f5c4e0fc1ced2cf37ec0f6082d15
Victor Limou <victorlimou@gmail.com>
2019-05-24 18:32:28 +0200

    feat(Interface): Add nav bar and style login/register

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,53 @@
 <template>
   <div>
     <!-- Pages content -->
-    <div class="container mt-3">
-      <div class="container my-2">
-        <b-button class="mr-2" :to="{ name: 'Home' }" size="sm">Home</b-button>
-        <b-button-group class="mr-2">
-          <b-button disabled size="sm">Logged out</b-button>
-          <b-button :to="{ name: 'Login' }" size="sm">Login</b-button>
-          <b-button :to="{ name: 'Register' }" size="sm">Register</b-button>
-        </b-button-group>
 
-        <b-button-group class="mr-2">
-          <b-button disabled size="sm">Logged in</b-button>
-          <b-button :to="{ name: 'Profile' }" size="sm">Profile</b-button>
-          <b-button :to="{ name: 'ProfileId', params: { id: '5ce5962e52d12e185cab5797' } }" size="sm">Profile/:id</b-button>
-        </b-button-group>
+    <!-- Header -->
+    <div class="app-header d-flex justify-content-start align-items-center bg-mauve fixed-top px-2">
+      <router-link :to="{ name: 'Home' }" class="mr-5">
+        <span class="app-title"  size="sm">KOFFEE'LEEIS</span>
+      </router-link>
 
-        <b-button @click="logout" class="mr-2" variant="info" size="sm" :disabled="!loggedIn">Your are logged {{ loggedIn ? 'in. Disconnect' : 'out' }}</b-button>
-      </div>
+      <!-- <b-button-group class="mr-2">
+        <b-button disabled size="sm">Logged out</b-button>
+        <b-button :to="{ name: 'Login' }" size="sm">Login</b-button>
+        <b-button :to="{ name: 'Register' }" size="sm">Register</b-button>
+      </b-button-group>
 
+      <b-button-group class="mr-2">
+        <b-button disabled size="sm">Logged in</b-button>
+        <b-button :to="{ name: 'Profile' }" size="sm">Profile</b-button>
+        <b-button :to="{ name: 'ProfileId', params: { id: '5ce5962e52d12e185cab5797' } }" size="sm">Profile/:id</b-button>
+      </b-button-group>-->
+
+      <b-button-group v-if="loggedIn && userData.role === 'exporter'" class="mr-auto align-self-stretch nav-container">
+        <!-- <b-button disabled size="sm">Exporter</b-button> -->
+        <b-button :to="{ name: 'Profile' }" size="sm" class="nav-btn">Orders</b-button>
+        <b-button :to="{ name: 'Profile' }" size="sm" class="nav-btn">Products</b-button>
+        <b-button :to="{ name: 'Profile' }" size="sm" class="nav-btn">Profile</b-button>
+
+      </b-button-group>
+
+      <b-button-group v-if="loggedIn && userData.role === 'importer'" class="mr-auto align-self-stretch nav-container">
+        <!-- <b-button disabled size="sm">Exporter</b-button> -->
+        <b-button :to="{ name: 'Profile' }" size="sm" class="nav-btn">Buy</b-button>
+        <b-button :to="{ name: 'Profile' }" size="sm" class="nav-btn">Open Orders</b-button>
+        <b-button :to="{ name: 'Profile' }" size="sm" class="nav-btn">Profile</b-button>
+
+      </b-button-group>
+
+      <b-button :to="{ name: 'Register' }" size="sm" v-if="!loggedIn" class="ml-auto mr-2" variant="light">Register</b-button>
+
+      <b-button :to="{ name: 'Login' }" size="sm" v-if="!loggedIn" class="btn-pink">Login</b-button>
+
+      <b-button @click="logout" class="ml-auto mr-2" variant="light" size="sm" v-if="loggedIn">
+        Disconnect
+      </b-button>
+    </div>
+    <div class="router-view-container">
       <transition name="fade" mode="out-in">
         <!-- View injected here -->
-        <router-view :key="$route.fullPath" />
+        <router-view :key="$route.fullPath"/>
         <!--/ View injected here -->
       </transition>
     </div>
@@ -30,15 +56,15 @@
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
-import './style.css'
+import { mapState, mapActions } from "vuex";
+import "./style.css";
 
 export default {
   computed: {
-    ...mapState(['loggedIn'])
+    ...mapState(["loggedIn","userData"])
   },
   methods: {
-    ...mapActions(['logout'])
+    ...mapActions(["logout"])
   }
-}
+};
 </script>
